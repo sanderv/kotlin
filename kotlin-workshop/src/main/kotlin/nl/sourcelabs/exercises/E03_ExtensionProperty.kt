55fc5ef75dcbe53796dcc67bcdbfe309b1b0667b
Sander Verbruggen <sander@verbruggen.ws>
2017-07-15 12:12:57 +0200

    ex 03

--- kotlin-workshop/src/main/kotlin/nl/sourcelabs/exercises/E03_ExtensionProperty.kt
+++ kotlin-workshop/src/main/kotlin/nl/sourcelabs/exercises/E03_ExtensionProperty.kt
@@ -1,5 +1,14 @@
 package nl.sourcelabs.exercises
 
+import java.math.BigDecimal
+
 /**
  * Write an extension property that easily converts an Int to a BigDecimal
  */
+
+val Int.bigDecimal
+    get() = BigDecimal(this)
+
+fun main(args: Array<String>) {
+    println(-3.bigDecimal)
+}
